1475f50e0110d9152e0897f2dff3fe3bd881e680
KLDAJI <kldaji@naver.com>
2022-02-15 20:24:38 +0900

    feat: ViewPager2 dot indicator

--- app/src/main/java/com/kldaji/android_medium/MainActivity.kt
+++ app/src/main/java/com/kldaji/android_medium/MainActivity.kt
@@ -2,10 +2,18 @@ package com.kldaji.android_medium
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.tabs.TabLayoutMediator
+import com.kldaji.android_medium.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val binding: ActivityMainBinding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        val mainViewPager2Adapter = MainViewPager2Adapter()
+        binding.vpMain.adapter = mainViewPager2Adapter
+        TabLayoutMediator(binding.tlMain, binding.vpMain) { _, _ -> }.attach()
     }
 }
