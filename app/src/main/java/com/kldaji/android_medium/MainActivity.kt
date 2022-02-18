8bcfd21aefaede8f93859d43848fc6e858e14302
KLDAJI <kldaji@naver.com>
2022-02-18 09:53:30 +0900

    feat: Cause memory leak

--- app/src/main/java/com/kldaji/android_medium/MainActivity.kt
+++ app/src/main/java/com/kldaji/android_medium/MainActivity.kt
@@ -1,11 +1,36 @@
 package com.kldaji.android_medium
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
+import android.graphics.BitmapFactory
+import android.os.AsyncTask
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.kldaji.android_medium.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    private var myAsyncTask: MyAsyncTask? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+
+        binding.btnMain.setOnClickListener {
+            if (myAsyncTask != null) {
+                finish()
+            }
+            myAsyncTask = MyAsyncTask(this)
+            myAsyncTask!!.execute()
+        }
+    }
+
+    inner class MyAsyncTask(private val context: Context) : AsyncTask<Unit, Unit, Unit>() {
+
+        override fun doInBackground(vararg params: Unit?) {
+            val bitmap =
+                BitmapFactory.decodeResource(context.resources, R.drawable.ic_launcher_background)
+            Thread.sleep(10000)
+        }
     }
 }
