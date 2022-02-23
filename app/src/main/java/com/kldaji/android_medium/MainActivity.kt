d6b96141e96160adb0d6b2930fb9a0afeb478aee
KLDAJI <kldaji@naver.com>
2022-02-23 22:03:24 +0900

    feat: ProgressBar Gauge

--- app/src/main/java/com/kldaji/android_medium/MainActivity.kt
+++ app/src/main/java/com/kldaji/android_medium/MainActivity.kt
@@ -1,11 +1,63 @@
 package com.kldaji.android_medium
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MotionEvent
+import com.kldaji.android_medium.databinding.ActivityMainBinding
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    private var gaugeUpTask: TimerTask? = null
+    private var gaugeDownTask: TimerTask? = null
+
+    @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        val timer = Timer()
+        var count = 0
+        binding.btnMain.setOnTouchListener { _, event ->
+
+            when (event.action) {
+                MotionEvent.ACTION_DOWN -> {
+                    gaugeUpTask = object : TimerTask() {
+                        override fun run() {
+                            if (count < 100) {
+                                count++
+                            }
+                            println(count)
+                            setGaugeProgress(count)
+                        }
+                    }
+                    timer.schedule(gaugeUpTask, 0, 20)
+                    true
+                }
+                MotionEvent.ACTION_MOVE -> false
+                MotionEvent.ACTION_UP -> {
+                    gaugeUpTask?.cancel()
+                    gaugeDownTask = object : TimerTask() {
+                        override fun run() {
+                            if (count == 0) {
+                                this.cancel()
+                            }
+                            count--
+                            println(count)
+                            setGaugeProgress(count)
+                        }
+                    }
+                    timer.schedule(gaugeDownTask, 0, 60)
+                    true
+                }
+                else -> false
+            }
+        }
+    }
+
+    fun setGaugeProgress(_progress: Int) {
+        if (_progress > 100) return
+        binding.progressBarMain.progress = _progress
     }
 }
